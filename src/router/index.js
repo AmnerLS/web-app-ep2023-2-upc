4d5b21e80b9617afc9a792b04dd5ca151430ebf9
AMNER LEVI LLAMO SANCHEZ <u20221c376@upc.edu.pe>
2024-05-07 18:37:37 -0500

    add page not found.

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import {createRouter, createWebHistory} from 'vue-router'
 import HomeComponent from "../public/pages/home.component.vue";
 import DrinkwareComponent from "../store/pages/drinkware.component.vue";
 import AccessoriesComponent from "../store/pages/accessories.component.vue";
+import PageNotFoundComponent from "../public/pages/page-not-found.component.vue";
 
 const router = createRouter({
     history: createWebHistory(),
@@ -9,7 +10,8 @@ const router = createRouter({
         { path:'/home', component: HomeComponent, meta:{ title: 'Home' } },
         { path:'/store/collections/all-drinkware',component: DrinkwareComponent, meta:{ title: 'Drinkware' }},
         { path:'/store/collections/accessories', component: AccessoriesComponent, meta:{ title: 'Accessories' }},
-        { path: "/", redirect: "home"}
+        { path: "/", redirect: "home"},
+        { path: '/:pathMatch(.*)*', component: PageNotFoundComponent, meta:{ title: 'Page Not Found' } }
     ]
 });
 
